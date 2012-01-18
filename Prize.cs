0e3b6881c7aaf7d4e32a62e943e178b8a1f245e4
Dominik <minikra@o2.pl>
2012-01-18 18:09:21 +0100

    prize!

---
+++ Prize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProfilesGenerator
+{
+    public class Prize
+    {
+        public List<String> Itemy;
+        public String Gold;
+        public String EXP;
+        public String PrizeID;
+
+        public Prize()
+        {
+            Itemy = new List<String>();
+            PrizeID = "pNowyPrize";
+            Gold = "0";
+            EXP = "0";
+        }
+    }
+}
